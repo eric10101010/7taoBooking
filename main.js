c6d101c7a10fd8f6b6327a0a71d35334a596f6f1
eric10101010 <x>
2023-09-26 22:07:37 +0800

    marquee

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 import './assets/scss/all.scss';
-
+import './node_modules/bootstrap/dist/js/bootstrap.bundle';
 $(document).ready(function(){
 	$('.hamburger').click(function(){
 		$(this).toggleClass('open');
 	});
 });
 
-
+const swiper = new Swiper('.marquee', {
+	loop: true,
+	speed: 8000,
+	slidesPerView: 'auto',    
+	autoplay: {
+		delay: 0,
+		pauseOnMouseEnter: true,        
+		disableOnInteraction: false,   
+		reverseDirection: true,         
+	},
+})
